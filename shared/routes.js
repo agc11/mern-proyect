4d4b4dadc06a504770d436f8e9fe316dadee5ec1
kuasi <adrian11gomez@gmail.com>
2016-03-16 21:15:34 +0100

    init

---
+++ shared/routes.js
@@ -0,0 +1,10 @@
+import { Route } from 'react-router';
+import React from 'react';
+import App from './container/App';
+
+const routes = (
+  <Route path="/" component={App} >
+  </Route>
+);
+
+export default routes;
